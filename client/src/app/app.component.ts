e79354df13f913b2e3f505923b19605c2c4bd8f9
David Lima <David.lima17@gmail.com>
2023-12-03 05:02:42 -0600

    End of Section 8

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Product } from './models/product';
+import { Pagination } from './models/pagination';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Skinet';
+  products: Product[] = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http
+      .get<Pagination<Product[]>>(
+        'https://localhost:5001/api/products?pageSize=50'
+      )
+      .subscribe({
+        next: (response: any) => {
+          this.products = response.data;
+        },
+        error: (error) => {
+          console.log(error);
+        },
+
+        // The complete callback is called when the observable completes and is no longer subscribed.
+        complete: () => {
+          console.log('request complete');
+          console.log('can make an extra statement here');
+        },
+      });
+  }
 }
