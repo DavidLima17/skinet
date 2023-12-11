76f27959193328561f98fa8bf042d97d5d5ab0a3
David Lima <David.lima17@gmail.com>
2023-12-11 13:15:03 -0600

    End of Section 12

--- client/src/app/core/core.module.ts
+++ client/src/app/core/core.module.ts
@@ -6,26 +6,28 @@ import { TestErrorComponent } from './test-error/test-error.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { ServerErrorComponent } from './server-error/server-error.component';
 import { Toast, ToastrModule } from 'ngx-toastr';
-
-
+import { SectionHeaderComponent } from './section-header/section-header.component';
+import { BreadcrumbModule } from 'xng-breadcrumb';
+import { NgxSpinnerModule } from 'ngx-spinner';
 
 @NgModule({
   declarations: [
     NavBarComponent,
     TestErrorComponent,
     NotFoundComponent,
-    ServerErrorComponent
+    ServerErrorComponent,
+    SectionHeaderComponent,
   ],
   imports: [
     CommonModule,
     RouterModule,
     ToastrModule.forRoot({
       positionClass: 'toast-bottom-right',
-      preventDuplicates: true
-    })
+      preventDuplicates: true,
+    }),
+    BreadcrumbModule,
+    NgxSpinnerModule,
   ],
-  exports: [
-    NavBarComponent
-  ]
+  exports: [NavBarComponent, SectionHeaderComponent, NgxSpinnerModule],
 })
-export class CoreModule { }
+export class CoreModule {}
