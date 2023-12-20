361131b23c33995c480e0e71e0526bef0b6d3d05
David Lima <David.lima17@gmail.com>
2023-12-20 15:32:32 -0600

    End of Section 18

---
+++ Core/Entities/OrderAggregate/Order.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Core.Entities.OrderAggregate
+{
+    public class Order : BaseEntity
+    {
+        public Order()
+        {
+        }
+        public Order(IReadOnlyList<OrderItem> orderItems, string buyerEmail, Address shipToAddress, DeliveryMethod deliveryMethod, decimal subtotal)
+        {
+            OrderItems = orderItems;
+            BuyerEmail = buyerEmail;
+            ShipToAddress = shipToAddress;
+            DeliveryMethod = deliveryMethod;
+            Subtotal = subtotal;
+        }
+
+        public string BuyerEmail { get; set; }
+        public DateTime OrderDate { get; set; } = DateTime.UtcNow; // default value
+        public Address ShipToAddress { get; set; }
+        public DeliveryMethod DeliveryMethod { get; set; }
+        public IReadOnlyList<OrderItem> OrderItems { get; set; } // read only list
+        public decimal Subtotal { get; set; }
+        public OrderStatus Status { get; set; } = OrderStatus.Pending; // default value
+        public string PaymentIntentId { get; set; } // for stripe
+
+        public decimal GetTotal()
+        {
+            return Subtotal + DeliveryMethod.Price;
+        }
+    }
+}
